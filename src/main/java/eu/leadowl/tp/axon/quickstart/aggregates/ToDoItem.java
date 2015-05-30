e67a470cc2185959e6daf4b831dad788bdb77572
leadowl <rushymek@gmail.com>
2015-05-30 10:11:38 +0200

    passing tests

--- src/main/java/eu/leadowl/tp/axon/quickstart/aggregates/ToDoItem.java
+++ src/main/java/eu/leadowl/tp/axon/quickstart/aggregates/ToDoItem.java
@@ -1,7 +1,35 @@
 package eu.leadowl.tp.axon.quickstart.aggregates;
 
+import org.axonframework.commandhandling.annotation.CommandHandler;
+import org.axonframework.eventhandling.annotation.EventHandler;
 import org.axonframework.eventsourcing.annotation.AbstractAnnotatedAggregateRoot;
+import org.axonframework.eventsourcing.annotation.AggregateIdentifier;
+
+import eu.leadowl.tp.axon.quickstart.commands.CreateToDoItemCommand;
+import eu.leadowl.tp.axon.quickstart.commands.MarkCompletedCommand;
+import eu.leadowl.tp.axon.quickstart.events.ToDoItemCompletedEvent;
+import eu.leadowl.tp.axon.quickstart.events.ToDoItemCreatedEvent;
 
 public class ToDoItem extends AbstractAnnotatedAggregateRoot {
+
+  @AggregateIdentifier
+  private String id;
+
+  public ToDoItem() {}
+
+  @CommandHandler
+  public ToDoItem(final CreateToDoItemCommand command) {
+    apply(new ToDoItemCreatedEvent(command.getTodoId(), command.getDescription()));
+  }
+
+  @EventHandler
+  public void on(final ToDoItemCreatedEvent event) {
+    id = event.getTodoId();
+  }
+
+  @CommandHandler
+  public void markCompleted(final MarkCompletedCommand command) {
+    apply(new ToDoItemCompletedEvent(id));
+  }
 
 }

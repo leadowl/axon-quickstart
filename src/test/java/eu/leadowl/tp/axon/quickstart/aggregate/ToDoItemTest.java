37beaa50ee56c249e0ea5647658d372f70dabe62
leadowl <rushymek@gmail.com>
2016-02-21 18:06:14 +0100

    formatting

--- src/test/java/eu/leadowl/tp/axon/quickstart/aggregate/ToDoItemTest.java
+++ src/test/java/eu/leadowl/tp/axon/quickstart/aggregate/ToDoItemTest.java
@@ -5,7 +5,6 @@ import org.axonframework.test.Fixtures;
 import org.junit.Before;
 import org.junit.Test;
 
-import eu.leadowl.tp.axon.quickstart.aggregate.ToDoItem;
 import eu.leadowl.tp.axon.quickstart.command.CreateToDoItemCommand;
 import eu.leadowl.tp.axon.quickstart.command.MarkCompletedCommand;
 import eu.leadowl.tp.axon.quickstart.event.ToDoItemCompletedEvent;
@@ -13,23 +12,23 @@ import eu.leadowl.tp.axon.quickstart.event.ToDoItemCreatedEvent;
 
 public class ToDoItemTest {
 
-  private FixtureConfiguration<ToDoItem> fixture;
+	private FixtureConfiguration<ToDoItem> fixture;
 
-  @Before
-  public void setUp() throws Exception {
-    fixture = Fixtures.newGivenWhenThenFixture(ToDoItem.class);
-  }
+	@Before
+	public void setUp() throws Exception {
+		fixture = Fixtures.newGivenWhenThenFixture(ToDoItem.class);
+	}
 
-  @Test
-  public void testCreateToDoItem() throws Exception {
-    fixture.given().when(new CreateToDoItemCommand("todo1", "need to implement the aggregate"))
-        .expectEvents(new ToDoItemCreatedEvent("todo1", "need to implement the aggregate"));
-  }
+	@Test
+	public void testCreateToDoItem() throws Exception {
+		fixture.given().when(new CreateToDoItemCommand("todo1", "need to implement the aggregate"))
+				.expectEvents(new ToDoItemCreatedEvent("todo1", "need to implement the aggregate"));
+	}
 
-  @Test
-  public void testMarkToDoItemAsCompleted() throws Exception {
-    fixture.given(new ToDoItemCreatedEvent("todo1", "need to implement the aggregate"))
-        .when(new MarkCompletedCommand("todo1")).expectEvents(new ToDoItemCompletedEvent("todo1"));
-  }
+	@Test
+	public void testMarkToDoItemAsCompleted() throws Exception {
+		fixture.given(new ToDoItemCreatedEvent("todo1", "need to implement the aggregate"))
+				.when(new MarkCompletedCommand("todo1")).expectEvents(new ToDoItemCompletedEvent("todo1"));
+	}
 
 }
